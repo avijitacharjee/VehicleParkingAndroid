2170c73048113582d68ef861b74f2d7ef0bf8fdb
AvijitAcharjee <avijitach@gmail.com>
2020-11-29 15:36:52 +0600

    maps integrated with directions in maps app]

--- app/src/main/java/com/avijit/vehicleparking/MainActivity.java
+++ app/src/main/java/com/avijit/vehicleparking/MainActivity.java
@@ -2,6 +2,7 @@ package com.avijit.vehicleparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,12 +13,19 @@ import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    ActivityMainBinding binding ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(ActivityMainBinding.inflate(getLayoutInflater()).getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         /*ImageView img = findViewById(R.id.image);
         Picasso.get().load("https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=Aviji").into(img);*/
-
+        binding.goBtn.setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(),MainDashboard.class));
+        });
+        binding.signupIntentButton.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),ActivitySignUp.class));
+        });
     }
 }
--- app/src/main/java/com/avijit/vehicleparking/MainDashboard.java
+++ app/src/main/java/com/avijit/vehicleparking/MainDashboard.java
@@ -2,15 +2,19 @@ package com.avijit.vehicleparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.avijit.vehicleparking.databinding.ActivityMainDashboardBinding;
 
 public class MainDashboard extends AppCompatActivity {
-
+    ActivityMainDashboardBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(ActivityMainDashboardBinding.inflate(getLayoutInflater()).getRoot());
+        binding = ActivityMainDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.getRoot().setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+        });
     }
 }
