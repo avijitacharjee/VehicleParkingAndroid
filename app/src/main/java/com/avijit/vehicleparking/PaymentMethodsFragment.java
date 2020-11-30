4b0d92a1d3116b6ecf6dfaf68dd56afb932813fc
AvijitAcharjee <avijitach@gmail.com>
2020-12-01 03:42:26 +0600

    booking activity class deleted

---
+++ app/src/main/java/com/avijit/vehicleparking/PaymentMethodsFragment.java
@@ -0,0 +1,31 @@
+package com.avijit.vehicleparking;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.avijit.vehicleparking.databinding.FragmentPaymentMethodsBinding;
+
+/**
+ * Created by Avijit Acharjee on 12/1/2020 at 3:34 AM.
+ * Email: avijitach@gmail.com.
+ */
+public class PaymentMethodsFragment extends Fragment {
+    FragmentPaymentMethodsBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentPaymentMethodsBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
