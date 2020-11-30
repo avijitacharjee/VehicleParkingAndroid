1d86cd5ade4efa581f667fe104796b9a4058150f
AvijitAcharjee <avijitach@gmail.com>
2020-12-01 02:06:29 +0600

    help fragment databinding

---
+++ app/src/main/java/com/avijit/vehicleparking/HelpFragment.java
@@ -0,0 +1,26 @@
+package com.avijit.vehicleparking;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.avijit.vehicleparking.databinding.ActivityHelpBinding;
+
+/**
+ * Created by Avijit Acharjee on 12/1/2020 at 2:04 AM.
+ * Email: avijitach@gmail.com.
+ */
+public class HelpFragment extends Fragment {
+    ActivityHelpBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = ActivityHelpBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+}
